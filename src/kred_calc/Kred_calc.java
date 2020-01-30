5c7955b388bde8f0a2e2b0b8013d048c910e3470
lad <metrix@ukr.net>
2020-01-30 14:37:53 +0200

    commit

---
+++ src/kred_calc/Kred_calc.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kred_calc;
+
+/**
+ *
+ * @author lad
+ */
+public class Kred_calc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here        
+        Kred_calculator frame = new Kred_calculator();
+        frame.setLocationRelativeTo(null);        
+        frame.setVisible(true); 
+        frame.setResizable(false);
+    }
+    
+}
